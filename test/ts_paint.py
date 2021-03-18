e9488370a819ccd8d734bde54a14523959371d3e
邹明哲 <zoumingzhe@qq.com>
2021-03-18 10:41:26 +0800

    Update ts_paint.py

--- test/ts_paint.py
+++ test/ts_paint.py
@@ -4,7 +4,6 @@ sys.path.insert(0, r'..\pyshredder')
 from pyshredder.core.paint import paint
 from ztools import fbasic
 
-fbasic.ensure('D:\\painting')
-paint.disks(70.0)
+paint.disks(40.0)
 
 input("按回车（Enter）继续")
